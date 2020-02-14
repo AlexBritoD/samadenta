5779f01f0abc24e8e0afd95e01a5dbd617450d7a
Juan <diaz_teran@hotmail.com>
2020-02-14 12:58:01 -0600

    landingpage

--- resources/views/landingpage/includes/head.blade.php
+++ resources/views/landingpage/includes/head.blade.php
@@ -1,9 +1,13 @@
 <title>Login - landingpage</title>
+    
     <link rel="apple-touch-icon" href="landingpage/images/ico/apple-icon-120.png">
+    
     <link rel="shortcut icon" type="image/x-icon" href="resources/views/landingpage//images/ico/favicon.ico">
-    <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700" rel="stylesheet">
+    
+    <link rel="stylesheet" href="public/admin/css1/style.css" />
+    
     <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
     
-    <link rel="stylesheet" href="views/landingpage/css/bootstrap.min.css" />
-
+    <link rel="stylesheet" href="public/admin/css/bootstrap.min.css" />
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
